02b89dc5f9f1cf10eb79c4515610b387425f17f4
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-07-19 21:10:28 +0800

    Sample_Code Updated

--- sample_code.php
+++ sample_code.php
@@ -1,6 +1,7 @@
 <?php
 
-//The file "sample_code" is part of my asset management system using Laravel. It shows the flow of how "Deny", "Tag as //returned" and "Approved" buttons work. If non-admin user creates request, the admin user has the option to "Deny" or //"Approve" the request. If the non-admin user's request is "Approved", the system automatically set the status of the //request to "Approved" and automatically look for an asset that is "Available" and with the right asset category. And //lastly, once the asset is returned back, the availabilty of the asset changes to "Available" and can be borrowed again by //other non-admin users.
+    //The file "sample_code" is part of my asset management system using Laravel. It shows the flow of how "Deny", "Tag as returned" and "Approved" buttons work. If non-admin user creates request, the admin user has the option to "Deny" or "Approve" the request. If the non-admin user's request is "Approved", the system automatically set the status of the request to "Approved" and automatically look for an asset that is "Available" and with the right asset category. And lastly, once the asset is returned back, the availabilty of the asset changes to "Available" and can be borrowed again by other non-admin users.
+
 
     // Request Various Status
     //1 = Pending
